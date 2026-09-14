f8e4c6ac5f42ca3c806dfe742b1bbc4b5e00bc43
Umar Azizadah <umar.azizadah@gmail.com>
2026-09-14 16:49:04 -0700

    feat: Add course aggregation with Instructor and Textbook

---
+++ src/part4Aggregation/Course.java
@@ -0,0 +1,49 @@
+package part4Aggregation;
+
+public class Course {
+    // Instance vars
+    private String courseName;
+    private Instructor instructor;
+    private Textbook textbook;
+
+    // Constructor
+    public Course(String courseName, Instructor instructor, Textbook textbook) {
+        this.courseName = courseName;
+        this.instructor = instructor;
+        this.textbook = textbook;
+    }
+
+    // Getters
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public Textbook getTextbook() {
+        return textbook;
+    }
+
+    // Setters
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setTextbook(Textbook textbook) {
+        this.textbook = textbook;
+    }
+
+    public void print() {
+        System.out.println(
+                "Course name: " + getCourseName()
+                        + ", Instructor: " + getInstructor().getFirstName() + " " + getInstructor().getLastName()
+                        + ", Textbook: " + getTextbook().getTitle()
+                        + ", Author: " + getTextbook().getAuthor());
+    }
+}
---
+++ src/part4Aggregation/Instructor.java
@@ -0,0 +1,42 @@
+package part4Aggregation;
+
+public class Instructor {
+    // instance vars
+    private String firstName;
+    private String lastName;
+    private String officeNumber;
+
+    // Constructor
+    public Instructor(String firstName, String lastName, String officeNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.officeNumber = officeNumber;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getOfficeNumber() {
+        return officeNumber;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setOfficeNumber(String officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+}
